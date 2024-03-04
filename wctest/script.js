b159b52664052d4f0672c94a4bb08b1a397365ae
Gustavo <gustavo9426@gmail.com>
2024-03-03 21:18:47 -0400

    sd

--- wctest/script.js
+++ wctest/script.js
@@ -237,7 +237,7 @@ function getSpriteScale (spriteWidth, spriteHeight, availableSpaceWidth, availab
 
 function resize(width, height) {
 	   //console.log(width,height)
-       this.scale.setGameSize(width*window.devicePixelRatio,height*window.devicePixelRatio)
+       this.scale.setGameSize(width,height)
        this.world.resize(width,height)
        this.scale.setUserScale(window.devicePixelRatio)
        this.scale.refresh()
