2df31de85011f13fbbd8f0a3db2ddb6a916bd3ca
Gustavo <gustavo9426@gmail.com>
2024-03-04 14:32:51 -0400

    dd

--- wctest/script.js
+++ wctest/script.js
@@ -3,6 +3,7 @@ var game = new Phaser.Game({
 	width: window.innerWidth,
 	height: window.innerHeight, 
 	renderer: Phaser.WEBGL,
+    resolution:window.devicePixelRatio,
 	state: this
 });
 
@@ -24,7 +25,7 @@ function preload() {
 this.game.plugins.add(SpinePlugin);
 	this.stage.disableVisibilityChange = true;
 	game.load.crossOrigin = 'anonymous';
-    this.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+    this.scale.scaleMode = Phaser.ScaleManager.NONE;
     this.game.renderer.autoResize = true
 
 	this.bgColor = "#D3D3D3"; //Background Color
@@ -238,14 +239,14 @@ function resize(width, height) {
 	   //console.log(width,height)
       //this.scale.updateScalingAndBounds()
       //this.scale.setGameSize(width,height)
-      this.game.renderer.resize(width, height);
+      this.game.renderer.resize(width*window.devicePixelRatio, height*window.devicePixelRatio);
 
             //  The Camera can never be smaller than the Game size
-     this.game.camera.setSize(width, height);
+     this.game.camera.setSize(width*window.devicePixelRatio, height*window.devicePixelRatio);
 
             //  This should only happen if the world is smaller than the new canvas size
      //this.game.world.resize(width, height);
-     this.world.resize(width,height)
+     this.world.resize(width*window.devicePixelRatio,height*window.devicePixelRatio)
       
       // this.scale.refresh()
 
@@ -1273,9 +1274,9 @@ function runVFXink(){
 function addClientToGroup(initialPosition,targetPosition,groupClients,type,isPlayer){
     var clientsFrame = ['Guke_waimaixiaoge','Guke_Nvshangren','Guke_laonainai','Guke_jianzhugongren','Guke_nvyouke','Guke_huanyaxiaonvhai']
     var client = this.game.add.spine(0, 0, this.rnd.pick(clientsFrame));
-    client.x = initialPosition[0]
-    client.y = initialPosition[1]
-    client.scale.set(0.28)
+    client.x = initialPosition[0]*window.devicePixelRatio
+    client.y = initialPosition[1]*window.devicePixelRatio
+    client.scale.set(0.28*window.devicePixelRatio)
     client.premultipliedAlpha = true
 
     var bubble = this.game.add.sprite(targetPosition, initialPosition[1], 'atlas', 'Bubble_Ordinary order.png');
